0f655569588cb82375b0977dfadf7126c6f0d8cb
joydas <joy600508@gmail.com>
2023-09-08 20:46:52 +0600

    localStorage add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -7,17 +7,32 @@ import AddTodo from './components/AddTodo'
 
 const App = () => {
   const [notes, setNotes] = useState([])
+
   const handelAddTodo = (note) => {
-    setNotes((prvNotes) => {
-      return [...prvNotes, { id: uuidv4(), note }]
-    })
+    setNotes((prevNotes) => {
+      const newNote = { id: uuidv4(), note };
+      const updatedNotes = [...prevNotes, newNote];
+      localStorage.setItem('notes', JSON.stringify(updatedNotes));
+      return updatedNotes;
+    });
     toast("✅ Added Successfully");
   }
+
   const removeNotes = (id) => {
     const filterNotes = notes.filter((note) => note.id !== id);
     setNotes(filterNotes);
-    toast("✅ deleted Successfully");
+    localStorage.setItem('notes', JSON.stringify(filterNotes)); // নোট ডেটা localStorage এ স্টোর করুন
+    toast("✅ Deleted Successfully");
   }
+
+  useEffect(() => {
+    const storedNotes = localStorage.getItem('notes');
+    if (storedNotes) {
+      setNotes(JSON.parse(storedNotes));
+    }
+  }, []);
+
+
   return (
     <div className='flex flex-col  justify-center items-center bg-gradient-to-r from-indigo-300 from-10% via-sky-300 via-30% to-sky-200 to-90% lg:max-w-[45rem] p-5 mx-auto w-screen h-screen rounded-sm sm:h-[36rem] '>
       <h1 className='text-base-100 text-2xl font-bold mb-5 underline'>Notes App</h1>
